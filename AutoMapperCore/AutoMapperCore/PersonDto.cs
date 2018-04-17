6ac36267b302e7a2a4ef7fa9874858dfdec07dea
Sean Lemster <sean.lemster@noa.nintendo.com>
2018-04-17 06:50:53 -0700

    Refactor classes out to separate files Refactor static/instance AutoMapper calls to separate methods

---
+++ AutoMapperCore/AutoMapperCore/PersonDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AutoMapperCore
+{
+    /// <summary>
+    /// Sample Class used for testing the features of AutoMapper 
+    /// </summary>
+    public class PersonDto
+    {
+        public string FirstName     { get; set; }
+        public string LastName      { get; set; }
+        public int Age              { get; set; }
+        public AddressDto Addresses { get; set; }
+
+
+    }
+}
--- AutoMapperCore/AutoMapperCore/Program.cs
+++ AutoMapperCore/AutoMapperCore/Program.cs
@@ -1,61 +1,65 @@
 ﻿using System;
+using AutoMapper;
 
 namespace AutoMapperCore
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main(string[] args) 
         {
             Console.WriteLine("This is a Console Application targeting .NET Core 2.0");
+
+            
+            RunStaticMapper();
+
+            RunInstanceMapper();
+
+            Console.ReadLine();
         }
-    }
+
+        private static void RunStaticMapper()   
+        {
+            // Static
+            Mapper.Initialize(cfg => {
+
+                //cfg.AddProfile<AppProfile>();
+
+                cfg.CreateMap<PersonDbo, PersonDto>();
+                cfg.CreateMap<PersonDto, PersonDbo>();
 
 
+            });
 
-    /// <summary>
-    /// Sample Class used for testing the features of AutoMapper 
-    /// </summary>
-    public class PersonDbo
-    {
-        public string FirstName     { get; set; }
-        public string LastName      { get; set; }
-        public int Age              { get; set; }
-        public AddressDbo Addresses { get; set; }
-        
-    }
-
-    public class AddressDbo
-    {
-        public string  Address1 { get; set; }
-        public string Address2  { get; set; }
-        public string City      { get; set; }
-        public string State     { get; set; }
-        public string Zip       { get; set; } 
-
-    }
+            var dest1 = Mapper.Map<PersonDbo, PersonDto>(new PersonDbo { FirstName = "Sean", LastName = "Lemster", Addresses = new AddressDbo { Address1 = "", Address2 = "", City = "Redmond", State = "WA", Zip = "98052" } });
+            var dest2 = Mapper.Map<PersonDto, PersonDbo>(new PersonDto { FirstName = "Sean", LastName = "Lemster", Addresses = new AddressDto { Address1 = "", Address2 = "", City = "Redmond", State = "WA", Zip = "98052" } });
 
 
+            Console.WriteLine(dest1.FirstName);
+            Console.WriteLine(dest2.FirstName);
+        }
 
-    /// <summary>
-    /// Sample Class used for testing the features of AutoMapper 
-    /// </summary>
-    public class PersonDto
-    {
-        public string FirstName     { get; set; }
-        public string LastName      { get; set; }
-        public int Age              { get; set; }
-        public AddressDto Addresses { get; set; }
+        private static void RunInstanceMapper() 
+        {
+            // Instance
+            var config = new MapperConfiguration(cfg => {
+
+                //cfg.AddProfile<AppProfile>();
+
+                cfg.CreateMap<PersonDbo, PersonDto>();
+                cfg.CreateMap<PersonDto, PersonDbo>();
+
+            });
 
 
-    }
+            var mapper2 = config.CreateMapper();    // or   var mapper2 = new Mapper(config);
 
-    public class AddressDto
-    {
-        public string  Address1 { get; set; }
-        public string Address2  { get; set; }
-        public string City      { get; set; }
-        public string State     { get; set; }
-        public string Zip       { get; set; } 
 
+            var dest3 = mapper2.Map<PersonDbo, PersonDto>(new PersonDbo { FirstName = "Kristine", LastName = "Lemster", Addresses = new AddressDbo { Address1 = "", Address2 = "", City = "Redmond", State = "WA", Zip = "98052" } });
+            var dest4 = mapper2.Map<PersonDto, PersonDbo>(new PersonDto { FirstName = "Kristine", LastName = "Lemster", Addresses = new AddressDto { Address1 = "", Address2 = "", City = "Redmond", State = "WA", Zip = "98052" } });
+
+
+            Console.WriteLine(dest3.FirstName);
+            Console.WriteLine(dest4.FirstName);
+        }
     }
 }
