da95ec06259c97ce23eebc45a933e35b49c642bd
Sean Lemster <sean.lemster@noa.nintendo.com>
2018-04-17 05:48:45 -0700

    Add Person and Address Classes

---
+++ AutoMapperCore/AutoMapperCore/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AutoMapperCore
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Console Application targeting .NET Core 2.0");
+        }
+    }
+
+
+
+    /// <summary>
+    /// Sample Class used for testing the features of AutoMapper 
+    /// </summary>
+    public class PersonDbo
+    {
+        public string FirstName     { get; set; }
+        public string LastName      { get; set; }
+        public int Age              { get; set; }
+        public AddressDbo Addresses { get; set; }
+        
+    }
+
+    public class AddressDbo
+    {
+        public string  Address1 { get; set; }
+        public string Address2  { get; set; }
+        public string City      { get; set; }
+        public string State     { get; set; }
+        public string Zip       { get; set; } 
+
+    }
+
+
+
+    /// <summary>
+    /// Sample Class used for testing the features of AutoMapper 
+    /// </summary>
+    public class PersonDto
+    {
+        public string FirstName     { get; set; }
+        public string LastName      { get; set; }
+        public int Age              { get; set; }
+        public AddressDto Addresses { get; set; }
+
+
+    }
+
+    public class AddressDto
+    {
+        public string  Address1 { get; set; }
+        public string Address2  { get; set; }
+        public string City      { get; set; }
+        public string State     { get; set; }
+        public string Zip       { get; set; } 
+
+    }
+}
